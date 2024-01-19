85a31f6fc16f7ee490e523c842da267a21e5ed49
Almaz Nurmanov <almazbrr@gmail.com>
2024-01-18 20:28:46 -0500

    done

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,19 @@
+fun generate(numRows: Int): List<List<Int>> {
+	val reslist = mutableListOf(mutableListOf(1))
+	for(i in 1 until numRows){
+		reslist.add(mutableListOf(1))
+		for (j in 1 until i+1){
+			if(j==i){
+				reslist[i].add(1)
+			}else{
+				reslist[i].add(reslist[i-1][j-1]+reslist[i-1][j])
+			}
+		}
+	}
+	return reslist.toList()
+}
 fun main() {
-    println("Hello World!")
+	println("Hello World!")
+	val retList=generate(6)
+	println(retList.toString())
 }
